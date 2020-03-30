f947ec2e9b8a75a594235cd039a70aface7efcbd
tuyendatma <62637890+tuyendatma@users.noreply.github.com>
2020-03-30 14:06:20 +0700

    30/03/2020

---
+++ JavaScript/JS/khai-bao-bien.js
@@ -0,0 +1,8 @@
+let x = 10;
+let y =20.5;
+let z = true;
+let t = "Hà Tịnh";
+document.write( '<br/>giá trị x là:'+ x);
+document.write( '<br/>giá trị y là:'+ y);
+document.write( '<br/>giá trị z là:'+ z);
+document.write( '<br/>giá trị t là:'+ t);
